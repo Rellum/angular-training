a9f1fc552b6521c0b0bb9ad433423eb3b0859ad4
Vaughan Muller <vaughan@everlytic.com>
2018-03-28 14:23:32 +0200

    Session5 Date formatting

--- Session5/hello-user/hello-user.component.js
+++ Session5/hello-user/hello-user.component.js
@@ -1,9 +1,6 @@
 app.component('helloUser', {
   templateUrl: 'hello-user/hello-user.template.html',
   controller: ['$scope', '$http', 'coolService', function ($scope, $http, coolService) {
-    var newName = coolService.changeName("simon")
-    console.log(newName)
-
-    $scope.newName = newName
+    $scope.birthday = new Date()
   }]
 })
