84da1bcae97e37f59775bb0c39b9094ad77a9236
Vaughan Muller <vaughan@everlytic.com>
2018-03-28 14:25:14 +0200

    Session5 Json formatter

--- Session5/hello-user/hello-user.component.js
+++ Session5/hello-user/hello-user.component.js
@@ -2,5 +2,10 @@ app.component('helloUser', {
   templateUrl: 'hello-user/hello-user.template.html',
   controller: ['$scope', '$http', 'coolService', function ($scope, $http, coolService) {
     $scope.birthday = new Date()
+
+    $scope.data = {
+      vaughan: 'muller',
+      nicole: 'something'
+    }
   }]
 })
