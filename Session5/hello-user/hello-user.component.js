6a1371089b811c210dae4e7ab959d61a7cb4602c
Vaughan Muller <vaughan@everlytic.com>
2018-03-28 13:27:18 +0200

    Session5.2

--- Session5/hello-user/hello-user.component.js
+++ Session5/hello-user/hello-user.component.js
@@ -1,12 +1,12 @@
 app.component('helloUser', {
   templateUrl: 'hello-user/hello-user.template.html',
   controller: ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
-    $timeout(function() {
-      $scope.test = 'timer done!'
-    }, 2000)
-
-    $scope.test = 'works?'
+    $http.get('http://www.findfreewifi.co.za/publicjson/locationsnear?lat=-26.04375187772609&lng=28.09076412071545')
+    .then(function (response) {
+      //success
+      console.log(response.data.data)
+    }, function (response) {
+      //failure
+    })
   }]
 })
-
-
